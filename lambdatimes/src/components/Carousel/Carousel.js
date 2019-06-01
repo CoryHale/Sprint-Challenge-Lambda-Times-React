9fa0acfd0a49889747eaf6588e2c83e2a8d62555
Cory Hale <Cory@CoryHalsMacbook.lan>
2019-06-01 12:58:48 -0600

    Finished MVP and started Carousel functionality

--- lambdatimes/src/components/Carousel/Carousel.js
+++ lambdatimes/src/components/Carousel/Carousel.js
@@ -1,27 +1,41 @@
 import React, { Component } from 'react';
 import { carouselData } from '../../data'
 // Complete this Carousel 
+
+let curImg = 0;
+
 export default class Carousel extends Component {
   constructor(props){
     super(props);
     this.state = {
-
+      images: [],
     }
   }
-  componentDidMount(){
 
+  componentDidMount(){
+    this.setState({ images: carouselData })
   }
 
   leftClick = () => {
-
+    if (curImg === 0) {
+      curImg = this.state.images.length - 1;
+    }
+    else {
+      curImg = curImg - 1;
+    }
   }
 
   rightClick = () => {
-
+    if (curImg === this.state.images.length - 1) {
+      curImg = 0;
+    }
+    else {
+      curImg = curImg + 1;
+    }
   }
 
   selectedImage = () => {
-    return <img src={} style={{display: 'block'}} />
+    return <img src={this.state.images[curImg]} style={{display: 'block'}} alt="" />
   }
   
   render(){
@@ -29,6 +43,7 @@ export default class Carousel extends Component {
       <div className="carousel">
         <div className="left-button" onClick={this.leftClick}>{"<"}</div>
         <div className="right-button" onClick={this.rightClick}>{">"}</div>
+        <img src={this.state.images[curImg]} style={{display: 'block'}} alt="" />
       </div>
     )
   }
