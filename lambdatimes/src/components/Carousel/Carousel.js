7bd671e0edf34ee80b76c5074db1116e0b975d61
Cory Hale <Cory@CoryHalsMacbook.lan>
2019-06-03 21:02:20 -0600

    Finished Carousel and fixed all warnings

--- lambdatimes/src/components/Carousel/Carousel.js
+++ lambdatimes/src/components/Carousel/Carousel.js
@@ -2,13 +2,12 @@ import React, { Component } from 'react';
 import { carouselData } from '../../data'
 // Complete this Carousel 
 
-let curImg = 0;
-
 export default class Carousel extends Component {
   constructor(props){
     super(props);
     this.state = {
       images: [],
+      curImg: 0,
     }
   }
 
@@ -17,33 +16,41 @@ export default class Carousel extends Component {
   }
 
   leftClick = () => {
-    if (curImg === 0) {
-      curImg = this.state.images.length - 1;
+    if (this.state.curImg === 0) {
+      this.setState({
+        curImg: this.state.images.length - 1,
+      })
     }
     else {
-      curImg = curImg - 1;
+      this.setState(prevState => ({
+          curImg: prevState.curImg - 1,
+      }))
     }
   }
 
-  rightClick = () => {
-    if (curImg === this.state.images.length - 1) {
-      curImg = 0;
+  rightClick = () => { 
+    if (this.state.curImg === this.state.images.length - 1) {
+      this.setState({
+        curImg: 0,
+      })
     }
     else {
-      curImg = curImg + 1;
+      this.setState(prevState => ({
+          curImg: prevState.curImg + 1,
+      }))
     }
   }
 
   selectedImage = () => {
-    return <img src={this.state.images[curImg]} style={{display: 'block'}} alt="" />
+    return <img src={this.state.images[this.state.curImg]} style={{display: 'block'}} alt="" />
   }
   
   render(){
     return (
       <div className="carousel">
         <div className="left-button" onClick={this.leftClick}>{"<"}</div>
+        `${this.selectedImage()}`
         <div className="right-button" onClick={this.rightClick}>{">"}</div>
-        <img src={this.state.images[curImg]} style={{display: 'block'}} alt="" />
       </div>
     )
   }
